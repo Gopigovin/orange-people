21526819f9656d7695fe2273a1ba91db7af61ced
Gopigovin <gopi.govindasamy@syncfusion.com>
2019-08-12 22:27:09 +0530

    adduser page added
    adduser page added

--- src/App/adduser.js
+++ src/App/adduser.js
@@ -1,227 +1,164 @@
 import React from "react";
-import serialize from 'form-serialize';
-import { Button, Form } from 'semantic-ui-react';
-import DatePicker from "react-datepicker";
-import 'react-notifications/lib/notifications.css';
-import "react-datepicker/dist/react-datepicker.css";
-import { NotificationContainer, NotificationManager } from 'react-notifications';
 
-const stayOptions = [
-  { key: 'ac', text: 'AC', value: 'AC' },
-  { key: 'non-ac', text: 'NON-AC', value: 'NON-AC' },
-]
-const profOptions = [
-  { key: 'study', text: 'Studying', value: 'Studying' },
-  { key: 'non-study', text: 'Working', value: 'Working' },
-]
+class RentBill extends React.Component {
+    constructor(props) {
 
-class AddUser extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sharingType: null
+        super(props);
     }
-  }
+    onRentBill = () => {
+        window.print();
+    }
 
+    render() {
+        return (
 
-  onAddUserSubmit = function (event) {
-    var forms = document.getElementsByClassName('needs-validation');
-    var validation = Array.prototype.filter.call(forms, function (form) {
+            <div class="content-body col-md-12 p-5">
+                <section class="card">
+                    <div id="invoice-template" class="card-body">
+                        <div class="invoice-company-header">
+                            <div>Bill Receipt</div>
+                        </div>
+                        <div id="invoice-customer-details col-md-12 col-sm-12" class="row pt-2">
+                            <div class="col-md-6 col-sm-6 text-center text-md-left">
+                                <ul class="px-0 list-unstyled">
+                                    <li class="text-bold-800">Mr. Bret Lezama</li>
+                                    <li>4879 Westfall Avenue,</li>
+                                    <li>Albuquerque,</li>
+                                    <li>New Mexico-87102.</li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6 col-sm-6 text-center text-md-right">
+                                <table className='recipt-column'>
+                                    <tr>
+                                        <td className="column-left">Bill Date</td>
+                                        <td>06/05/2016</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="column-left">Terms</td>
+                                        <td>Due on Receipt</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="column-left">Due Date</td>
+                                        <td>10/05/2016</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
 
-      debugger;
-      if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-      if (form.checkValidity() === true) {
-        event.preventDefault();
-        var form = document.querySelector('#data');
-        document.querySelector('#loading').classList.add("loading");
-        var addData = serialize(form, { hash: true });
-        addData["key"] = "user";
-        addData["status"] = "active";
-        addData["uniqueID"] = "ORG" + Math.random().toString().substr(2, 4);
-        fetch('http://localhost:3000/add', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            addData
-          })
-        }).then(response => response.json())
-          .then(function (data) {
+                        <div id="invoice-items-details" class="pt-2">
+                            <div class="row">
+                                <div class="table-responsive col-sm-12">
+                                    <table class="recipt-details-table">
+                                        <thead>
+                                            <tr>
+                                                <th>Receipt No</th>
+                                                <th>Item &amp; Description</th>
+                                                <th class="text-right">Amount</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td scope="row">1</td>
+                                                <td>
+                                                    <p>Create PSD for mobile APP</p>
+                                                    <p class="text-muted">Simply dummy text of the printing and typesetting industry.</p>
+                                                </td>
 
-            if (data.status == 204) {
+                                                <td class="text-right">$ 2400.00</td>
+                                            </tr>
+                                            <tr>
+                                                <td scope="row">2</td>
+                                                <td>
+                                                    <p>iOS Application Development</p>
+                                                    <p class="text-muted">Pellentesque maximus feugiat lorem at cursus.</p>
+                                                </td>
 
-            } else {
-              debugger
-              document.querySelector('#loading').classList.remove("loading");
-              form.classList.remove('was-validated');
-              NotificationManager.success(' User added successfully', 'Success');
-              form.reset();
-            }
+                                                <td class="text-right">$ 6500.00</td>
+                                            </tr>
+                                            <tr>
+                                                <td scope="row">3</td>
+                                                <td>
+                                                    <p>WordPress Template Development</p>
+                                                    <p class="text-muted">Vestibulum euismod est eu elit convallis.</p>
+                                                </td>
 
-          }.bind(this)
-          ).catch(function (error) {
-
-
-          }.bind(this));
-      }
-      form.classList.add('was-validated');
-    }.bind(this));
-
-  }.bind(this);
-
-
-  // componentDidMount() {    
-  //     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  //     var forms = document.getElementsByClassName('needs-validation');
-  //     // Loop over them and prevent submission
-  //     var validation = Array.prototype.filter.call(forms, function (form) {
-  //       form.addEventListener('submit', function (event) {
-  //         debugger;
-  //         if (form.checkValidity() === false) {
-  //           event.preventDefault();
-  //           event.stopPropagation();
-  //         }
-  //         if (form.checkValidity() === true) {
-  //           debugger
-  //          this.onAddUserSubmit();
-  //         }
-  //         form.classList.add('was-validated');
-  //       }, false);
-  //     }.bind(this));    
-  // }
-
-  onSharingChange = (e, data) => {
-    this.setState({ sharingType: data.value });
-  }
-
-  onProfessiontypeChange = (e, data) => {
-    console.log(data.value);
-    this.setState({ professionType: data.value });
-  }
-
-  handleChange = (date) => {
-    this.setState({
-      startDate: date
-    });
-  }
-  render() {
-    return (
-      <div>
-        <div id="results" className="search-results p-5">
-        <NotificationContainer/>
-          <div>
-            <Form id='loading'>
-              <form id="data" onSubmit={this.onAddUserSubmit} className="form-group needs-validation" noValidate>
-                
-                <div className="form-group">
-                  <label for="exampleInputEmail1"> Name</label>
-                  <input type="" required className="form-control" placeholder="Enter name" name="name"></input>
-                  <div className="invalid-feedback">
-                    Please enter a username.
-                   </div>
-                  {/* <div className="valid-feedback">
-        Looks good!
-      </div> */}
-                </div>
-                <div className="form-group">
-                  <label for="exampleInputEmail1"> Email</label>
-                  <input type="" required className="form-control" placeholder="Enter email" name="email"></input>
-                  <div className="invalid-feedback">
-                    Please enter a email.
-                   </div>
-                </div>
-                <div className="row">
-                  <div className="form-group col-md-6">
-                    <label for="exampleInputEmail1">Hostel No</label>
-                    <input type="" required className="form-control" placeholder="Enter hostel number" name="hostelno"></input>
-                    <div className="invalid-feedback">
-                      Please enter a hostel number.
-                   </div>
-                  </div>
-                  <div className="form-group col-md-6">
-                    <label for="exampleInputEmail1">Room No</label>
-                    <input type="" required className="form-control" placeholder="Enter room number" name="roomno"></input>
-                    <div className="invalid-feedback">
-                      Please enter a room number.
-                   </div>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="form-group col-md-6">
-                    <label for="exampleInputEmail1">Sharing (AC/ Non-A/C)</label>
-                    <Form.Select fluid onChange={this.onSharingChange} options={stayOptions} placeholder="Sharing (AC/ Non-A/C)" />
-                    <input type="" required value={this.state.sharingType} className="form-control  d-none" placeholder="Email" name="sharingtype"></input>
-                    <div className="invalid-feedback">
-                      Please enter a sharing mode.
-                   </div>
-                  </div>
-                  <div className="form-group col-md-6">
-                    <label for="exampleInputEmail1">Studying / Working</label>
-                    <Form.Select fluid onChange={this.onProfessiontypeChange} options={profOptions} placeholder="Studying / Working" />
-
-                    <input required className="form-control  d-none" placeholder="Email" name="professiontype" value={this.state.professionType}></input>
-                    <div className="invalid-feedback">
-                      Please enter a profession type.
-                   </div>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="form-group col-md-6">
-                    <label for="exampleInputEmail1">Advance Amount</label>
-                    <input type required className="form-control" placeholder="Enter the advance amount" name="advanceamount"></input>
-                    <div className="invalid-feedback">
-                      Please enter the advance amount
-                   </div>
-                  </div>
-                  <div className="form-group col-md-6">
-                    <label for="exampleInputEmail1">Date of Joining</label>
-                    <DatePicker selected={this.state.startDate}
-                      onChange={this.handleChange} required={true} className="col-md-12" name="doj" placeholderText="Enter the Joining Date"></DatePicker>
-                    <div className="invalid-feedback">
-                      Please enter the date of Joining
-                   </div>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="form-group col-md-6">
-                    <label for="exampleInputEmail1">Mobile Number</label>
-                    <input type="" required className="form-control" placeholder="Email" name="smobileno"></input>
-                    <div className="invalid-feedback">
-                      Please enter the mobile number
-                   </div>
-                  </div>
-                  <div className="form-group col-md-6">
-                    <label for="exampleInputEmail1">Parent's Mobile Number</label>
-                    <input type="" required className="form-control" placeholder="Email" name="pmobileno"></input>
-                    <div className="invalid-feedback">
-                      Please enter the Parents amount
-                   </div>
-                  </div>
-                </div>
-                <div className="form-group">
-                  <label for="exampleInputEmail1">Address</label>
-                  <textarea required className="form-control" placeholder="Enter permanent address" rows="3" name="address"></textarea>
-                  <div className="invalid-feedback">
-                    Please enter the permanent address
-                   </div>
-                </div>
+                                                <td class="text-right">$ 6000.00</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="container">
+                            <div class="row">
+                                <div class="payment-mode col-sm">
+                                    <p class="lead">Payment Methods:</p>
+                                    <table class="table table-borderless table-sm">
+                                        <tbody>
+                                            <tr>
+                                                <td>Bank name:</td>
+                                                <td>ABC Bank, USA</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Acc name:</td>
+                                                <td>Amanda Orton</td>
+                                            </tr>
+                                            <tr>
+                                                <td>IBAN:</td>
+                                                <td>FGS165461646546AA</td>
+                                            </tr>
+                                            <tr>
+                                                <td>SWIFT code:</td>
+                                                <td>BTNPP34</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="payment-mode col-sm">
+                                    <p class="lead">Total due</p>
+                                    <table class="table">
+                                        <tbody>
+                                            <tr>
+                                                <td>Sub Total</td>
+                                                <td>$ 14,900.00</td>
+                                            </tr>
+                                            <tr>
+                                                <td>TAX (12%)</td>
+                                                <td>$ 1,788.00</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="text-bold-800">Total</td>
+                                                <td class="text-bold-800"> $ 16,688.00</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Payment Made</td>
+                                                <td class="pink">(-) $ 4,688.00</td>
+                                            </tr>
+                                            <tr class="bg-grey bg-lighten-4">
+                                                <td class="text-bold-800">Balance Due</td>
+                                                <td class="text-bold-800">$ 12,000.00</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <div class="">
+                                        <p>Authorized person</p>
+                                        {/* <img src="../../../app-assets/images/pages/signature-scan.png" alt="signature" class="height-100"/> */}
+                                        <h6>Amanda Orton</h6>
+                                        <p class="text-muted">Managing Director</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
                 <div className="text-center">
-                  <input type="submit" value="Add" className="btn btn-outline-primary w-25" />
+                    <button className="ui primary button  btn-sm m-1 osw-print" onClick={this.onRentBill}>Print</button>
                 </div>
+            </div>
 
-              </form>
-            </Form>
-          </div>
-        </div>
-
-      </div>
-    );
-  }
+        );
+    }
 };
 
 
-export default AddUser
+export default RentBill
